17f343ee78ce57315e20c9da408cb8050e5013fc
backmeupplz <backmeupplz@gmail.com>
2021-04-05 08:38:03 -0700

    add index

--- src/models/user.ts
+++ src/models/user.ts
@@ -10,7 +10,7 @@ export class User {
   @prop({ required: true, index: true, unique: true })
   token: string
 
-  @prop()
+  @prop({ index: true })
   subscriptionId?: string
   @prop({ required: true, default: false })
   free: boolean
