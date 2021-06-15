68978078db2fc62f443e43459e2e827f50f0da4e
backmeupplz <backmeupplz@gmail.com>
2021-06-15 16:43:52 -0700

    add invite route

---
+++ src/controllers/invite.ts
@@ -0,0 +1,24 @@
+import { DocumentType } from '@typegoose/typegoose'
+import { Controller, Post, Body, Flow, Ctx } from 'koa-ts-controllers'
+import { Context } from 'koa'
+import { authenticate } from '@/middlefares/authenticate'
+import { User, UserModel } from '@/models/user'
+
+@Controller('/invite')
+export default class InfoController {
+  @Post('/code')
+  @Flow(authenticate)
+  async postInviteCode(
+    @Ctx() ctx: Context,
+    @Body('inviteCode') inviteCode: string
+  ) {
+    const inviter = await UserModel.findOne({ inviteCode })
+    if (!inviter) {
+      return ctx.throw(404)
+    }
+    const user = ctx.state.user as DocumentType<User>
+    user.inviter = inviter
+    await user.save()
+    ctx.status = 200
+  }
+}
